1d34ce179211252dd3cf0725e08ac00f8764b0d3
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-03-07 13:55:12 +0200

    fix ts for good

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,27 @@ const diceImg = <img className='diceImg' src="src\assets\icon-dice.svg" alt='dic
 
 function App() {
   const [count, setCount] = useState([])
+  const [advice, setAdvice] = useState([])
  
+  
 
   const hook = () => {
     axios
     .get("https://api.adviceslip.com/advice")
     .then(response =>{
-      setCount(response["data"]["slip"])
+      setCount(response["data"]["slip"]["id"])
+      setAdvice(response["data"]["slip"]["advice"])
     })
   }
   useEffect(hook, [])
+
  
   return (
     <div className="App">
 
       <div className='main-element'>
-      <div className='id'>Advice #{count["id"]}</div>
-      <div className='advice'>{count["advice"]}</div>
+      <div className='id'>Advice #{count}</div>
+      <div className='advice'>{advice}</div>
       <img className='pattern' src='src\assets\pattern-divider-desktop.svg' alt="patter-divider"></img>
       <button className='rndBtn' onClick={hook}>{diceImg}</button>
       </div>
